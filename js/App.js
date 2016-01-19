eae6508a9983f059cc395560624c2c9dac6cb237
alexurquhart <alexurquhart@gmail.com>
2016-01-19 19:57:42 +0000

    Updated example site

--- js/App.js
+++ js/App.js
@@ -9,7 +9,6 @@
 if (!window.location.origin) {
 	window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
-alert(window.location.origin)
 
 $(document).ready(function() {
 	
